5e08c65a75899539662e354267ac87682b8065bb
aloysxgame200-prog <aloysxgame200@gmail.com>
2026-05-28 18:22:27 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -737,6 +737,21 @@ async def classement(interaction: discord.Interaction):
         )
 
     await interaction.response.send_message(embed=embed)
+# ==========================================
+#   SERVEUR WEB POUR RENDER / UPTIMEROBOT
+# ==========================================
 
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot en ligne !")
+
+def run_web():
+    port = int(os.environ.get("PORT", 8080))
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
+
+Thread(target=run_web, daemon=True).start()
 
 bot.run(TOKEN)
